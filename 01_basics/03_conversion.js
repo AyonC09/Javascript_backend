64a883ea695f31d6c3b2d7b6afe6467281958424
AyonC09 <112944984+AyonC09@users.noreply.github.com>
2026-06-07 08:12:39 +0000

    conversion of data types

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,40 @@
+/*let score ="Ayon"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber = Number(score);//rely less on number
+console.log(typeof valueInNumber);//it should not convert but it converts
+console.log(valueInNumber);//NaN special type
+
+let score1 = null
+
+console.log(typeof score1)//object
+console.log(typeof(score1))
+
+let valueInNumber1 = Number(score1);//rely less on number
+console.log(typeof valueInNumber1);
+
+let score2 = undefined
+
+console.log(typeof score2)//undefined
+console.log(typeof(score2))
+
+let valueInNumber2 = Number (score2);//rely less on number
+console.log(typeof valueInNumber2);//number
+*/
+/*
+"33" => 33
+"33abc" => NaN
+1 => true
+"" => false
+"Afd" => true
+*/
+let isLoggedIn = "A"
+let boolisLoggedIn = Boolean(isLoggedIn)
+//console.log(boolisLoggedIn)
+
+let number = 67
+let stringnumber = String(number)
+console.log(stringnumber)//67
+console.log(typeof stringnumber)
