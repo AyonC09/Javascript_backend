e1e3b4eaf760ff2b3483ccc320b0a0ee05dc7d83
AyonC09 <112944984+AyonC09@users.noreply.github.com>
2026-06-07 07:45:46 +0000

    codeconfig

---
+++ 02_datatypes/dtype.js
@@ -0,0 +1,19 @@
+"use strict";// treat all JS code as newer version
+
+//alert("Yo!bro")//we are using node environment and not browser
+console.log(3+3)
+
+// number => 2 to pow 53
+//bigInt ,string=>""
+//boolean => true/false
+//null => standlone value (representation)
+// undefined => var declared but not assigned
+//symbol => unique
+
+
+//object
+
+console.log(typeof"Ayon")
+console.log(typeof null)//object
+
+console.log(typeof undefined)//undefined
