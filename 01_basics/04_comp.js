7c660279e652311bb160d1a9b2446c2ae3a452da
AyonC09 <112944984+AyonC09@users.noreply.github.com>
2026-06-15 17:50:17 +0000

    code config

---
+++ 01_basics/04_comp.js
@@ -0,0 +1,14 @@
+// console.log("2" > 1)
+// console.log("02" > 1)
+console.log("0" <= null)
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)//value conversion is different or unpredictable
+
+console.log(undefined > 0)
+console.log(undefined == 0)
+console.log(undefined < 0)
+
+console.log("2" === 2)// do not convert
+console.log("02" == 2)//converts
