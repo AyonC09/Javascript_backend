0e57c7b30c1b717d89fdafa554b636d4895f3194
AyonC09 <112944984+AyonC09@users.noreply.github.com>
2026-06-17 14:04:02 +0000

    code config

--- 01_basics/04_comp.js
+++ 01_basics/04_comp.js
@@ -4,11 +4,14 @@ console.log("0" <= null)
 
 console.log(null > 0)
 console.log(null == 0)
-console.log(null >= 0)//value conversion is different or unpredictable
+console.log(null >= 0)
+//numeric coercion for relational operator occurs but not for equality opeartor
 
 console.log(undefined > 0)
 console.log(undefined == 0)
 console.log(undefined < 0)
+
+console.log(null == undefined)//bcz acc. to author it means absence of value and special case of JS
 
 console.log("2" === 2)// do not convert
 console.log("02" == 2)//converts
