4f8f3990589681476ad350de7969080a7927b798
AyonC09 <112944984+AyonC09@users.noreply.github.com>
2026-06-18 11:41:09 +0000

    code config

---
+++ 01_basics/07number_maths.js
@@ -0,0 +1,34 @@
+const score =100
+//console.log(score)
+const balance = new Number(300)
+//console.log(balance)
+//console.log(typeof(balance))//object
+
+//console.log(balance.toString().length)
+console.log(balance.toFixed(1))
+
+const otherNumber = 123.8966
+
+console.log(otherNumber.toPrecision(4))
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-In'))
+
+//++++++++++++++++++Maths++++++++++++++++++++
+
+console.log(Math)
+console.log(Math.abs(-4))
+console.log(Math.round(5.7))
+console.log(Math.ceil(4.4))
+console.log(Math.floor(4.5))
+//min(4,3,6,77) , max(3,4,5,67)
+
+console.log(Math.random())
+console.log((Math.random()*10) + 1)
+console.log(Math.round(Math.random()*10))
+//random() returns a value between 0 and 1
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max-min + 1)) + min)
