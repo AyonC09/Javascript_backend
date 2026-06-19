5d9b908e06f028e9547f39036138808b7050be49
AyonC09 <112944984+AyonC09@users.noreply.github.com>
2026-06-19 09:34:42 +0000

    code config

---
+++ 02_basics/array2.js
@@ -0,0 +1,28 @@
+const marvel_heros = ['thor','Ironman','spiderman']
+const dc_heros = ['superman','batman','flash']
+
+
+// marvel_heros.push(dc_heros)
+// console.log(marvel_heros)
+
+
+// const combinedArr = marvel_heros.concat(dc_heros)
+// console.log(combinedArr)
+
+const combinedArr = [...marvel_heros,...dc_heros]
+console.log(combinedArr)
+
+const anotherArr = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+
+const usableanother_Arr = anotherArr.flat(Infinity)
+console.log(usableanother_Arr)
+
+console.log(Array.isArray("Ayon"))
+console.log(Array.from("Ayon"))
+console.log(Array.from({name:"Ayon"})) //interesting case []
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1,score2,score3))
