7c4e34cd5d79a79385d605d834b5cfdd3ba0c9bb
AyonC09 <112944984+AyonC09@users.noreply.github.com>
2026-06-22 18:10:23 +0000

    code config

--- 03_basics/04iife.js
+++ 03_basics/04iife.js
@@ -1,9 +1,9 @@
 //Immediately Invoked Function Expression(IIFE)
-
+//Named IIFE
 (function chai(){
     console.log(`DB CONNECTED`)
 })();
-
+// Unnamed IIFE
 ((name) =>{
     console.log(`DB CONNECTED TO ${name}`)
 })('Ayon');
