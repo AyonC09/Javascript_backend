2bfc93b4ba421cd318ecc15dab08aca10da64a04
AyonC09 <112944984+AyonC09@users.noreply.github.com>
2026-06-22 17:39:10 +0000

    code config

--- 03_basics/02scopes.js
+++ 03_basics/02scopes.js
@@ -4,7 +4,51 @@ if(true){
 let a = 10
 const b = 20
 //var c = 30
+//console.log("INNER:",a);
 }
-console.log(a)
+//console.log(a)
 //console.log(b)
 //console.log(c)// (var c) 30 is showing the output of it's scope
+
+
+function one(){
+    const username = "ayon"
+
+    function two(){
+        const website = "youtube"
+        console.log(username)
+    }
+   // console.log(website);
+
+    two()
+}
+
+//one()
+
+if(true){
+    const username = "ayon"
+    if(username === "ayon"){
+        const website = "youtube"
+        console.log(username + website)
+    }
+   // console.log(website)
+}
+
+//console.log(username)
+
+
+
+// ++++++++++++++++interesting+++++++++++++++++++++
+
+console.log(addone(7))
+function addone(num){
+    return num + 1
+}
+
+
+
+//console.log(addTwo(7))// error occurs bcz declaration is after the print
+const addTwo = function(num){
+    return num + 2
+}
+
