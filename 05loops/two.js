9bf8ad92d19bb58549f28595fb453a44f08fee1f
AyonC09 <112944984+AyonC09@users.noreply.github.com>
2026-06-23 10:19:07 +0000

    code config

---
+++ 05loops/two.js
@@ -0,0 +1,20 @@
+let index = 0 
+while (index <= 10){
+    console.log(`Value of index:${index}`)
+    index += 2
+}
+
+const myarray = [4,6,178,8,9]
+let i = 0
+while(i != myarray.length){
+    console.log(`Value of the array is ${myarray[i]}`)
+    i++;
+}
+
+
+let score = 1
+
+do{
+    console.log(`Score is ${score}`)
+    score++
+}while(score<=10)
