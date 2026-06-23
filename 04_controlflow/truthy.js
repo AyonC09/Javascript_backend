41b6a13c82561a2c11477f911282f761472cd17d
AyonC09 <112944984+AyonC09@users.noreply.github.com>
2026-06-23 09:38:36 +0000

    code config

---
+++ 04_controlflow/truthy.js
@@ -0,0 +1,35 @@
+const userEmail = " "
+
+if(userEmail){
+    console.log("Got User Email");
+}else{
+    console.log("Don't have user email")
+}
+
+//falsy values
+//false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+//truthy values
+//[], {}, "0", "false", " ", function(){}
+
+const user = []
+
+if(user.length === 0){
+    console.log("Empty Array")
+}
+
+const emptyObj = {}
+
+if(Object.keys(emptyObj).length === 0){
+    console.log("Object is empty")
+}
+
+//Nullish Coalescing Operator(??): null undefined
+
+let val1;
+//val1 = 5 ?? 10 //5
+//val1 = null??10 //10
+//val1 = undefined??10//10
+//val1 = undefined ?? null //null
+val1 = null ?? undefined //undefined
+console.log(val1)
